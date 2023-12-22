9a60936989c805600cef5259b09083e9e462794f
刘博 <liubo1@gg.com>
2023-12-22 11:02:51 +0800

    [新增] dist前端编译文件

--- backend/main.py
+++ backend/main.py
@@ -55,4 +55,4 @@ def read_activities():
     return jsonify(list(activities))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000, debug=True)
+    app.run(host='0.0.0.0')
--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://10.10.20.24:8000', // 替换为你的后端地址
+    baseURL: 'http://liuboo.pythonanywhere.com/', // 替换为你的后端地址
 });
 export default instance;
-
