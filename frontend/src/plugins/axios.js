58ca034b1de4b45282f96520d7109f9f3aa3cfdb
刘博 <liubo1@gg.com>
2023-12-26 15:32:09 +0800

    [修改] 服务器地址为阿里云

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://liuboo.pythonanywhere.com/', // 替换为你的后端地址
+    baseURL: 'http://172.23.139.224:5000', // 替换为你的后端地址
 });
 export default instance;
